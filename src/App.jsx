e84bddcf218b3efaad439906f85878ad3c02480c
Timineri <tonyeokoko@outlook.com>
2026-02-05 05:10:34 +0100

    Implemented Suspense with Lazy Loading.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
+import { lazy } from "react";
 import { Routes, Route } from "react-router";
-import Home from "./Pages/Home";
-import Details from "./Pages/Detail";
-import ErrorPage from "./Pages/Error";
+let Home = lazy(() => import('./Pages/Home'))
+let Details = lazy (() => import('./Pages/Detail'))
+let ErrorPage = lazy (() => import('./Pages/Error'))
 import './index.css';
 
 export default function App() {
 
   return (
     <>
-      {/* <NavBar/> */}
       <Routes>
         <Route path ="/" element={<Home/>}/>
-        {/* <Route path ="/details" element={<Details/>}/> */}
         <Route path="/details/:body" element={<Details />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+import { StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -7,7 +7,9 @@ import { BrowserRouter } from 'react-router'
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <StrictMode>
+    <Suspense fallback= {<div>Loading...</div>}>
     <App />
+    </Suspense>
   </StrictMode>
   </BrowserRouter>
 )
