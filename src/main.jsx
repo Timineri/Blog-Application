d31332cab1e958299952b0b483d0042a482d9eb9
Timineri <tonyeokoko@outlook.com>
2026-02-05 17:09:15 +0100

    Implemented ErrorBoundary.

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,27 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router'
+import { ErrorBoundary} from 'react-error-boundary'
+
+function ErrorFallback ({error, resetErrorBoundary}){
+  return (
+    <div> 
+      <h2>Yikes! Something went wrong </h2> <p>{error.message}</p>
+      <button onclick={resetErrorBoundary}> Try Again </button>
+    </div>
+  )
+
+}
+
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <StrictMode>
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
     <Suspense fallback= {<div>Loading...</div>}>
     <App />
     </Suspense>
+    </ErrorBoundary>
   </StrictMode>
   </BrowserRouter>
 )
