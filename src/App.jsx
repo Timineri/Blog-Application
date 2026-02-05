99f3eeb8930981a9bd97bf69633b81d68233aeac
Timineri <tonyeokoko@outlook.com>
2026-02-05 04:31:27 +0100

    Included the Error 404 Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router";
 import Home from "./Home";
 import Details from "./Detail";
 import NavBar from "./NavBar";
+import ErrorPage from "./Error";
 import './index.css';
 
 export default function App() {
@@ -13,6 +14,7 @@ export default function App() {
         <Route path ="/" element={<Home/>}/>
         {/* <Route path ="/details" element={<Details/>}/> */}
         <Route path="/details/:id" element={<Details />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </>
   )
